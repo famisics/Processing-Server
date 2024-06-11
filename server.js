074870fe6d932cb4091bbb1c39b21a30fb5a7d6b
famisics <famisics@uiro.dev>
2024-06-11 23:50:56 +0900

    feat: テキストが復元されるようになったはず

--- server.js
+++ server.js
@@ -15,15 +15,14 @@ wss.on('connection', (ws) => {
   clients.push(ws);
   console.log('[wss] ローカルサーバーが接続されました');
   ws.on('message', (message) => {
-    console.log('[LOCAL>WSS]', 'receive' , message);
     clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send('DELIVER:'+message);
-        console.log('[WSS>LOCAL]', 'deliver', message);
+        console.log('[WSS>PROC]', 'deliver', message.toString());
       }
       if (client == ws && client.readyState === WebSocket.OPEN) {
         client.send('SUCCESS:'+message);
-        console.log('[WSS>LOCAL]', 'success', message);
+        console.log('[WSS>PROC]', 'success', message.toString());
       }
     });
   });
